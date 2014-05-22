890ddf27554c6af16662487319a3c8ce04f628c7
Barn18 <gui.lagrange18@hotmail.com>
2014-05-22 09:09:00 -0400

    main.c functions

--- Labo-wiznet.X/main.c
+++ Labo-wiznet.X/main.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <plib/spi.h>
+#include "socket.h"
 
 #define W5500_RDY_PORT  PORTD
 #define W5500_RDY_PIN   7
@@ -23,12 +24,58 @@
 #pragma config  OSC = INTIO67, BOREN = OFF, PWRT = ON
 #pragma config  WDT = OFF, DEBUG = ON, LVP = OFF
 
+intr_kind wiznet_interrupt = 0;
+char wiznet_interrupt_flag = 0;
+
+void wizchip_cris_enter(void);
+void wizchip_cris_exit(void);
+void wizchip_cs_select(void);
+void wizchip_cs_deselect(void);
+uint8_t wizchip_spi_readbyte(void);
+void wizchip_spi_writebyte(uint8_t wb);
+
+void interrupt ISR() {
+    if (INT0IF == 1) {
+        INT0IF = 0;
+        wiznet_interrupt = wizchip_getinterrupt();
+
+    }
+}
+
 int main(int argc, char** argv) {
     OSCCON = 0b01110010;
     ADCON1 = 0xFF;
     TRISC = (1 << 4);
-    TRISD = (1 << 5);
+    TRISB = (1 << 0);
+    wizchip_cris_enter();
+    wizchip_cs_deselect();
+    OpenSPI(SPI_FOSC_4, MODE_00, SMPMID);
+    GIE = 1;
+
 
     return (EXIT_SUCCESS);
 }
 
+void wizchip_cris_enter(void) {
+    INT0IE = 1;
+}
+
+void wizchip_cris_exit(void) {
+    INT0IE = 0;
+}
+
+void wizchip_cs_select(void) {
+    LATD &= ~(1 << 4);
+}
+
+void wizchip_cs_deselect(void) {
+    LATD |= 1 << 4;
+}
+
+uint8_t wizchip_spi_readbyte(void) {
+    return ReadSPI();
+}
+
+void wizchip_spi_writebyte(uint8_t wb) {
+    WriteSPI(wb);
+}
